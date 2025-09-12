60c735d56f7ce18345d874ed8425348f7e7ad599
Guilherme Cruz <guiga2922@gmail.com>
2025-09-12 16:29:48 -0300

    Altera urls.py para que css do admin funcione corretametne

--- app/urls.py
+++ app/urls.py
@@ -11,9 +11,13 @@ urlpatterns = [
     path('login/', login_view, name='login'),
     path('logout/', logout_view, name='logout'),
     path('cars/', CarsListView.as_view(), name='cars_list'),
-    path('new_car/', NewCarCreateView.as_view(), 
-    name='new_car'),
+    path('new_car/', NewCarCreateView.as_view(), name='new_car'),
     path('car/<int:pk>/', CarDetailView.as_view(), name='car_detail'),
     path('car/<int:pk>/update/', CarUpdateView.as_view(), name='car_update'),
     path('car/<int:pk>/delete/', CarDeleteView.as_view(), name='car_delete'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
